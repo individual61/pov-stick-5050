e563a19d45033b20f06474a8d6fa84c29a63efa7
individual61 <individual61@users.noreply.github.com>
2020-09-25 13:09:31 -0700

    Can now do arbitrarily-wide image 1 and image 1 in Stick 1

--- Code/Rev A board/src/main.cpp
+++ Code/Rev A board/src/main.cpp
@@ -3,25 +3,19 @@
 // #define BUILD_STICK_2
 //#define BUILD_STICK_3
 
-
-
-                                                                                                               
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ 
 //                                                                                                                 
 //
-//                                                                                                                 
-//      ██████   ██████  ██    ██     ███████ ████████ ██  ██████ ██   ██     ███████  ██████  ███████  ██████     
-//      ██   ██ ██    ██ ██    ██     ██         ██    ██ ██      ██  ██      ██      ██  ████ ██      ██  ████    
-//      ██████  ██    ██ ██    ██     ███████    ██    ██ ██      █████       ███████ ██ ██ ██ ███████ ██ ██ ██    
-//      ██      ██    ██  ██  ██           ██    ██    ██ ██      ██  ██           ██ ████  ██      ██ ████  ██    
-//      ██       ██████    ████       ███████    ██    ██  ██████ ██   ██     ███████  ██████  ███████  ██████     
-//                                                                                                                 
-//                                                                                                                 
-//                                                                                                                                                                                                                                
+//
+//      ██████   ██████  ██    ██     ███████ ████████ ██  ██████ ██   ██     ███████  ██████  ███████  ██████ 
+//      ██   ██ ██    ██ ██    ██     ██         ██    ██ ██      ██  ██      ██      ██  ████ ██      ██  ████ 
+//      ██████  ██    ██ ██    ██     ███████    ██    ██ ██      █████       ███████ ██ ██ ██ ███████ ██ ██ ██ 
+//      ██      ██    ██  ██  ██           ██    ██    ██ ██      ██  ██           ██ ████  ██      ██ ████  ██ 
+//      ██       ██████    ████       ███████    ██    ██  ██████ ██   ██     ███████  ██████  ███████  ██████  
+//                                                                                                             
+//
+//
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ 
-                                                                                                               
-
-  
 
 /* 
 Description:	POV Stick 5050: A persistence of vision stick with an AtTiny85, 10 APA102C RGB LEDs, one button, and a surprise.
@@ -60,10 +54,8 @@ Hardware:		The GitHub repo has KiCAD board files and a mostly-correct BOM. Three
 
 
  */
-                                                        
-//  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ 
-     
 
+//  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ 
 
 #include <Arduino.h>
 #include <FastLED.h>
@@ -86,28 +78,23 @@ CRGB leds[NUMPIXELS];
 // Green coef is blue
 // therefore blue is red
 
-                                                        
-                                                        
-                                                       
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
 //                                                           
-//  
-//                                                         
-//                               ██                          
-//                              ███                          
-//                               ██                          
-//                               ██                          
-//                               ██                          
-//                                                           
-//                                                           
-//                                                                                                                      
-//  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
-//                                                                                                                            
-//                   failed penis, constant flashing 
 //
-#ifdef BUILD_STICK_1 
-
-
+//
+//                               ██ 
+//                              ███ 
+//                               ██ 
+//                               ██ 
+//                               ██ 
+//                                  
+//
+//
+//  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
+//                                                           
+//                   failed penis, constant flashing
+//
+#ifdef BUILD_STICK_1
 
 uint32_t prog1_sweepInterval = 0;
 uint32_t prog1_lastTimeSweep = 0;
@@ -141,30 +128,35 @@ uint8_t column_index = 0;
 uint8_t row_index = 0;
 void program_1(void)
 {
-	rainbowStart = rainbowStart -1;
-	FastLED.setBrightness(180);
+	// note that the rainbow advances on every sweep, not on every individual image, and even if we are in image2
+	rainbowStart = rainbowStart - 1;
+
 	fill_rainbow(leds, NUMPIXELS, rainbowStart, rainbowIncrement);
-	for(int row_index = 0; row_index < 10; row_index++)
+	for (int row_index = 0; row_index < NUMPIXELS; row_index++)
 	{
 		//image[column_index*IMAGE_WIDTH + row_index]; argument will go from 0 to (IMAGE_WIDTH*IMAGE_HEIGHT -1)
 		// with row_index from 0 to (IMAGE_HEIGHT - 1);
 		// column_index from 0 to (IMAGE_WIDTH - 1)
+
 		uint8_t image_value = 0;
-		if(displayAlternateImage == 0)
+		// Image 1
+		if (displayAlternateImage == 0)
 		{
-      	FastLED.setBrightness(120);
+			FastLED.setBrightness(120);
 			image_value = pgm_read_byte(&(image1[column_index][row_index]));
 		}
-		else
+
+		// Image 2
+		if (displayAlternateImage == 1)
 		{
-      	FastLED.setBrightness(180);
+			FastLED.setBrightness(180);
 			image_value = pgm_read_byte(&(image2[column_index][row_index]));
 		}
-		if(image_value==0 )
+		if (image_value == 0)
 		{
 			leds[row_index] = CRGB(0, 0, 0);
 		}
-	/*	if(image_value==2 )
+		/*	if(image_value==2 )
 		{
 			leds[row_index] = CRGB(60, 60,60);
 		}
@@ -172,30 +164,46 @@ void program_1(void)
 		{
 			leds[row_index] = CRGB(130, 0, 130);
 		}*/
-  if(image_value==3 )
-    {
-      leds[row_index] = CRGB(255, 255,255);
-    }
-
+		if (image_value == 3)
+		{
+			leds[row_index] = CRGB(255, 255, 255);
+		}
 	}
 	column_index++;
 	// image done
-	if(column_index == IMAGE1_WIDTH)
+
+	// If we are displaying image 2 and are at the end, turn off displayAlternateImage.
+	// This should be the only place where this is changed back to 0 (image 1).
+	// checkButton() is the only place where it can be set to 1 (image 2).
+	if (displayAlternateImage == 1)
 	{
-    if(displayAlternateImage == 1)
-    {
-      displayAlternateImage = 0;
-    }
-		column_index = 0;;
+		if (column_index == IMAGE2_WIDTH)
+		{
+			displayAlternateImage = 0;
+			column_index = 0;
+		}
+	}
+	if (displayAlternateImage == 0)
+	{
+		if (column_index == IMAGE1_WIDTH)
+		{
+			column_index = 0;
+		}
 	}
 };
+
+void setup_1(void)
+{
+	FastLED.setBrightness(180);
+}
 
 void loop_1()
 {
 	timeNow = millis();
 	checkButton();
-	
-	if(timeNow - prog1_lastTimeSweep > prog1_sweepInterval)
+
+	// Update lights only if we have waited longer than prog1_sweepInterval
+	if (timeNow - prog1_lastTimeSweep > prog1_sweepInterval)
 	{
 		prog1_lastTimeSweep = timeNow;
 		program_1();
@@ -203,25 +211,23 @@ void loop_1()
 	}
 }
 
-#endif // #ifdef BUILD_STICK_1                                                   
+#endif // #ifdef BUILD_STICK_1
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
 //                                                           
-//      
-//                                                     
-//                              ██████                       
-//                                   ██                      
-//                               █████                       
-//                              ██                           
-//                              ███████                      
-//                                                           
-//                                                           
-//                                                                                                                   
+//
+//
+//                              ██████ 
+//                                   ██ 
+//                               █████  
+//                              ██     
+//                              ███████ 
+//                                      
+//
+//
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
-//                                                                                                                         
-//                      SLOW RAINBOW THEN HEARTS                                       
+//                                                           
+//                      SLOW RAINBOW THEN HEARTS
 #ifdef BUILD_STICK_2
-
-
 
 uint32_t prog1_sweepInterval = 0;
 uint32_t prog1_lastTimeSweep = 0;
@@ -239,28 +245,30 @@ uint8_t column_index = 0;
 uint8_t row_index = 0;
 void program_1(void)
 {
-	rainbowStart = rainbowStart -1;
+	rainbowStart = rainbowStart - 1;
 	FastLED.setBrightness(180);
 	fill_rainbow(leds, NUMPIXELS, rainbowStart, rainbowIncrement);
-	for(int row_index = 0; row_index < 10; row_index++)
+	for (int row_index = 0; row_index < 10; row_index++)
 	{
-		//image[column_index*IMAGE_WIDTH + row_index]; argument will go from 0 to (IMAGE_WIDTH*IMAGE_HEIGHT -1)
+		// image[column_index*IMAGE_WIDTH + row_index]; 
+		// argument will go from 0 to (IMAGE_WIDTH*IMAGE_HEIGHT -1)
 		// with row_index from 0 to (IMAGE_HEIGHT - 1);
 		// column_index from 0 to (IMAGE_WIDTH - 1)
 		uint8_t image_value = pgm_read_byte(&(image1[column_index][row_index]));
-		if(image_value==0 )
+		if (image_value == 0)
 		{
 			leds[row_index] = CRGB(0, 0, 0);
 		}
 		//  else
 		//  {
-//      leds[row_index] = CRGB(32, 0, 32);
+		//      leds[row_index] = CRGB(32, 0, 32);
 		//  }
 	}
 	column_index++;
-	if(column_index == IMAGE1_WIDTH)
+	if (column_index == IMAGE1_WIDTH)
 	{
-		column_index = 0;;
+		column_index = 0;
+		;
 	}
 };
 
@@ -271,13 +279,17 @@ void program_2(void)
 	fill_rainbow(leds, NUMPIXELS, rainbowStart, rainbowIncrement);
 }
 
+void setup_2(void)
+{
+}
+
 void loop_2(void)
 {
-    buttonState = digitalRead(BUTTON1);
+	buttonState = digitalRead(BUTTON1);
 
-	if(!buttonState)
+	if (!buttonState)
 	{
-		if(timeNow - prog1_lastTimeSweep > prog1_sweepInterval)
+		if (timeNow - prog1_lastTimeSweep > prog1_sweepInterval)
 		{
 			prog1_lastTimeSweep = timeNow;
 			program_1();
@@ -286,7 +298,7 @@ void loop_2(void)
 	}
 	else
 	{
-		if(timeNow - prog2_lastTimeSweep > prog2_sweepInterval)
+		if (timeNow - prog2_lastTimeSweep > prog2_sweepInterval)
 		{
 			prog2_lastTimeSweep = timeNow;
 			program_2();
@@ -295,56 +307,51 @@ void loop_2(void)
 	}
 }
 
-
-
-                                                                
-//                                                           
-#endif // #ifdef BUILD_STICK_2                                                       
+//
+#endif // #ifdef BUILD_STICK_2 
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
 //                                                           
-//        
-//                                                   
-//                              ██████                       
-//                                   ██                      
-//                               █████                       
-//                                   ██                      
-//                              ██████                       
-//                                                           
-//                                                           
-//                                                                                                         
+//
+//
+//                              ██████ 
+//                                   ██ 
+//                               █████  
+//                                   ██ 
+//                              ██████  
+//                                     
+//
+//
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
 #ifdef BUILD_STICK_3                                                      
-                                     
 
-
+void setup_3(void)
+{
+}
 
 void loop_3(void)
 {
-
-
 }
 
-#endif // #ifdef BUILD_STICK_3                                                        
+#endif // #ifdef BUILD_STICK_3
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
 //                                                           
-//       
-//                                                    
-//              ███    ███  █████  ██ ███    ██              
-//              ████  ████ ██   ██ ██ ████   ██              
-//              ██ ████ ██ ███████ ██ ██ ██  ██              
-//              ██  ██  ██ ██   ██ ██ ██  ██ ██              
-//              ██      ██ ██   ██ ██ ██   ████              
-//                                                           
-//                                                           
-//                                                                                                                 
+//
+//
+//              ███    ███  █████  ██ ███    ██ 
+//              ████  ████ ██   ██ ██ ████   ██ 
+//              ██ ████ ██ ███████ ██ ██ ██  ██ 
+//              ██  ██  ██ ██   ██ ██ ██  ██ ██ 
+//              ██      ██ ██   ██ ██ ██   ████ 
+//                                              
+//
+//
 //  ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██
 //                                                           
-//                                          
-
+//
 
 void setup()
 {
-	if(F_CPU == 16000000)
+	if (F_CPU == 16000000)
 	{
 		clock_prescale_set(clock_div_1);
 	}
@@ -355,15 +362,27 @@ void setup()
 	digitalWrite(HEARTBEAT_LED, HIGH);
 
 	FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUMPIXELS)
-	.setCorrection(CORRECTION);
+		.setCorrection(CORRECTION);
 
-	for(int i = 0; i < NUMPIXELS; i++)
+	for (int i = 0; i < NUMPIXELS; i++)
 	{
 		leds[i] = CRGB(16, 0, 16);
 	}
 	FastLED.setBrightness(10);
 	FastLED.show();
 
+// After the common setup has been run, run stick-specific setup.
+#ifdef BUILD_STICK_1
+	setup_1();
+#endif
+
+#ifdef BUILD_STICK_2
+	setup_2();
+#endif
+
+#ifdef BUILD_STICK_3
+	setup_3();
+#endif
 }
 
 void loop()
@@ -371,19 +390,18 @@ void loop()
 	timeNow = millis();
 
 	// Beat heartbeat LED
-	if(heartbeatLed == 1)
+	if (heartbeatLed == 1)
 	{
-		if(timeNow - lastTimeHeartbeatOn > heartbeatOnInterval)
+		if (timeNow - lastTimeHeartbeatOn > heartbeatOnInterval)
 		{
 			lastTimeHeartbeatOff = timeNow;
 			heartbeatLed = 0;
 			digitalWrite(HEARTBEAT_LED, LOW);
-
 		}
 	}
-	if(heartbeatLed == 0)
+	if (heartbeatLed == 0)
 	{
-		if(timeNow - lastTimeHeartbeatOff > heartbeatOffInterval)
+		if (timeNow - lastTimeHeartbeatOff > heartbeatOffInterval)
 		{
 			lastTimeHeartbeatOn = timeNow;
 			heartbeatLed = 1;
@@ -391,17 +409,16 @@ void loop()
 		}
 	}
 
-
+// After common loop instructions, run stick-specific loop.
 #ifdef BUILD_STICK_1
-loop_1();
+	loop_1();
 #endif
 
 #ifdef BUILD_STICK_2
-loop_2();
+	loop_2();
 #endif
 
 #ifdef BUILD_STICK_3
-loop_3();
+	loop_3();
 #endif
-
 }
