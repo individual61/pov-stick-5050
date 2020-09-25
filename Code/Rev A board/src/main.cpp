e728c434a1ab234abf4356d721d54fab16ed6540
individual61 <individual61@users.noreply.github.com>
2020-09-25 00:28:24 -0700

    Gave up and put everything in one file. Compiles.

--- Code/Rev A board/src/main.cpp
+++ Code/Rev A board/src/main.cpp
@@ -3,6 +3,85 @@
 // #define BUILD_STICK_2
 //#define BUILD_STICK_3
 
+// typeface is BVig Full Fitted
+//http://patorjk.com/software/taag/#p=display&h=0&v=1&c=c%2B%2B&w=%20&f=Big&t=................%0A%20%20%20%20%20%20main%0A................
+
+
+/*
+   ICSP header is, looking down on pins (not looking into female cable):
+
+   1  MISO    2 VCC
+   3 SCK      4 MOSI
+   5 RST      6 GND
+
+   ATTiny85:
+
+   1 RST   5 VCC
+   2 NC    6 SCK
+   3 NC    7 MISO
+   4 GND   8 MOSI
+
+ */
+
+#include <Arduino.h>
+#include <FastLED.h>
+#include <avr/power.h>
+//#include "parameters_1.h"
+//#include "parameters_2.h"
+#include <parameters.h>
+
+
+
+
+
+// with BGR
+// Red coef is green
+// Green coef is blue
+// therefore blue is red
+
+
+uint32_t timeNow = 0;
+
+uint32_t heartbeatOffInterval = HEARTBEAT_OFF_INTERVAL;
+uint32_t heartbeatOnInterval = HEARTBEAT_ON_INTERVAL;
+
+uint32_t lastTimeHeartbeatOn = 0;
+uint32_t lastTimeHeartbeatOff = 0;
+
+uint8_t heartbeatLed = 0;
+
+CRGB leds[NUMPIXELS];
+
+//                                                                  
+//                                                                  
+//                                                                  
+//                                                                  
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//
+//                                  __                              
+//                                 /_ |                             
+//                                  | |                             
+//                                  | |                             
+//                                  | |                             
+//                                  |_|                             
+//
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//                                                                  
+// 
+
+
+
+#ifdef BUILD_STICK_1
+
+//#include "parameters_common.h"
+//#include "parameters_1.h"
+
+
+// failed penis, constant flashing 
+
+
 
 
 /*
@@ -22,61 +101,165 @@
  */
 
 
-
-
-#include <Arduino.h>
-#include <FastLED.h>
-#include <avr/power.h>
-#include "stick1/parameters_1.h"
-#include "stick2/parameters_2.h"
-#include "parameters_common.h"
-
-// STICK 2 PROGRAM: SLOW RAINBOW THEN HEARTS
-
-CRGB leds[NUMPIXELS];
-
 // with BGR
 // Red coef is green
 // Green coef is blue
-// therefore blue is red
+//therefore blue is red
 
 
-uint32_t timeNow = 0;
-
-uint32_t heartbeatOffInterval = HEARTBEAT_OFF_INTERVAL;
-uint32_t heartbeatOnInterval = HEARTBEAT_ON_INTERVAL;
-
-uint32_t lastTimeHeartbeatOn = 0;
-uint32_t lastTimeHeartbeatOff = 0;
-
-uint8_t heartbeatLed = 0;
+//uint32_t timeNow = 0; //prob dont need to uncomment if reverting
 
 
+uint32_t prog1_sweepInterval = 0;
+uint32_t prog1_lastTimeSweep = 0;
 
-void setup()
+uint32_t prog2_sweepInterval = 3;
+uint32_t prog2_lastTimeSweep = 0;
+
+
+
+
+
+
+
+uint8_t buttonPrevState = 0;
+uint8_t buttonState = 0;
+uint32_t timeNowbutton = 0;
+uint32_t lastTimePressedButton = 0;
+uint8_t displayAlternateImage = 0;
+void checkButton(void)
 {
-	if(F_CPU == 16000000)
+	bool buttonState = digitalRead(BUTTON1);
+	if (buttonState == 0) // pressed
 	{
-		clock_prescale_set(clock_div_1);
+		timeNowbutton = millis();
+		if (timeNowbutton - lastTimePressedButton > DEBOUNCEDELAY)
+		{
+			displayAlternateImage = 1;
+			lastTimePressedButton = timeNowbutton;
+		}
 	}
-
-	pinMode(HEARTBEAT_LED, OUTPUT);
-	pinMode(BUTTON1, INPUT);
-
-	digitalWrite(HEARTBEAT_LED, HIGH);
-
-	FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUMPIXELS)
-	.setCorrection(CORRECTION);
-
-	for(int i = 0; i < NUMPIXELS; i++)
-	{
-		leds[i] = CRGB(16, 0, 16);
-	}
-	FastLED.setBrightness(10);
-	FastLED.show();
-
 }
 
+
+//int8_t sweepIndex = 0;
+int8_t sweepDir = 1;
+uint8_t rainbowIncrement = 7;
+uint8_t rainbowStart = 0;
+uint8_t column_index = 0;
+uint8_t row_index = 0;
+void program_1(void)
+{
+	rainbowStart = rainbowStart -1;
+	FastLED.setBrightness(180);
+	fill_rainbow(leds, NUMPIXELS, rainbowStart, rainbowIncrement);
+	for(int row_index = 0; row_index < 10; row_index++)
+	{
+		//image[column_index*IMAGE_WIDTH + row_index]; argument will go from 0 to (IMAGE_WIDTH*IMAGE_HEIGHT -1)
+		// with row_index from 0 to (IMAGE_HEIGHT - 1);
+		// column_index from 0 to (IMAGE_WIDTH - 1)
+		uint8_t image_value = 0;
+		if(displayAlternateImage == 0)
+		{
+      	FastLED.setBrightness(120);
+			image_value = pgm_read_byte(&(image1[column_index][row_index]));
+		}
+		else
+		{
+      	FastLED.setBrightness(180);
+			image_value = pgm_read_byte(&(image2[column_index][row_index]));
+		}
+		if(image_value==0 )
+		{
+			leds[row_index] = CRGB(0, 0, 0);
+		}
+	/*	if(image_value==2 )
+		{
+			leds[row_index] = CRGB(60, 60,60);
+		}
+		if(image_value==1 )
+		{
+			leds[row_index] = CRGB(130, 0, 130);
+		}*/
+  if(image_value==3 )
+    {
+      leds[row_index] = CRGB(255, 255,255);
+    }
+
+	}
+	column_index++;
+	// image done
+	if(column_index == IMAGE1_WIDTH)
+	{
+    if(displayAlternateImage == 1)
+    {
+      displayAlternateImage = 0;
+    }
+		column_index = 0;;
+	}
+};
+
+
+
+void loop_1()
+{
+	timeNow = millis();
+	checkButton();
+	
+	if(timeNow - prog1_lastTimeSweep > prog1_sweepInterval)
+	{
+		prog1_lastTimeSweep = timeNow;
+		program_1();
+		FastLED.show();
+	}
+}
+
+
+
+
+#endif
+
+
+
+
+
+
+//                                                                  
+//                                                                  
+//                                                                  
+//                                                                  
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//
+//                                  ___                             
+//                                 |__ \                            
+//                                    ) |                           
+//                                   / /                            
+//                                  / /_                            
+//                                 |____|   
+//                        
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//                                                                  
+//                                                                  
+
+
+
+#ifdef BUILD_STICK_2
+
+
+//#include "parameters_common.h"
+//#include "parameters_2.h"
+
+// STICK 2 PROGRAM: SLOW RAINBOW THEN HEARTS
+
+uint32_t prog1_sweepInterval = 0;
+uint32_t prog1_lastTimeSweep = 0;
+
+uint32_t prog2_sweepInterval = 3;
+uint32_t prog2_lastTimeSweep = 0;
+
+uint8_t buttonState = 0;
 
 int8_t sweepIndex = 0;
 int8_t sweepDir = 1;
@@ -111,7 +294,118 @@ void program_1(void)
 	}
 };
 
-extern void loop_1();
+
+void program_2(void)
+{
+	FastLED.setBrightness(10);
+	rainbowStart = rainbowStart - 1;
+	fill_rainbow(leds, NUMPIXELS, rainbowStart, rainbowIncrement);
+}
+
+void loop_2(void)
+{
+    buttonState = digitalRead(BUTTON1);
+
+	if(!buttonState)
+	{
+		if(timeNow - prog1_lastTimeSweep > prog1_sweepInterval)
+		{
+			prog1_lastTimeSweep = timeNow;
+			program_1();
+			FastLED.show();
+		}
+	}
+	else
+	{
+		if(timeNow - prog2_lastTimeSweep > prog2_sweepInterval)
+		{
+			prog2_lastTimeSweep = timeNow;
+			program_2();
+			FastLED.show();
+		}
+	}
+
+
+}
+
+#endif
+
+//                                                                  
+//                                                                  
+//                                                                  
+//                                                                  
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//                                  ____                            
+//                                 |___ \                           
+//                                   __) |                          
+//                                  |__ <                           
+//                                  ___) |                          
+//                                 |____/                           
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//                                                                  
+//                                          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                                                                  
+//                                                                  
+//                                                                  
+//                                                                  
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//
+//                                    _                             
+//                                   (_)                            
+//                _ __ ___     __ _   _   _ __                      
+//               | '_ ` _ \   / _` | | | | '_ \                     
+//               | | | | | | | (_| | | | | | | |                    
+//               |_| |_| |_|  \__,_| |_| |_| |_|      
+//
+//    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _ 
+//   (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_) (_)
+//                                                                  
+//                                           
+
+
+void setup()
+{
+	if(F_CPU == 16000000)
+	{
+		clock_prescale_set(clock_div_1);
+	}
+
+	pinMode(HEARTBEAT_LED, OUTPUT);
+	pinMode(BUTTON1, INPUT);
+
+	digitalWrite(HEARTBEAT_LED, HIGH);
+
+	FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUMPIXELS)
+	.setCorrection(CORRECTION);
+
+	for(int i = 0; i < NUMPIXELS; i++)
+	{
+		leds[i] = CRGB(16, 0, 16);
+	}
+	FastLED.setBrightness(10);
+	FastLED.show();
+
+}
 
 
 void loop()
